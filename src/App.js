9fa409129aeca222243b52ab375d591754dd5b8a
Felix-fale <falefelix23@gmail.com>
2023-01-24 01:51:16 +0000

    feat: Setting up the menu items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import OptionSelection from './components/OptionSelection'
+import { arrayItems } from './AIOptions';
 
 
 function App() {
-  console.log(process.env.REACT_APP_OPEINAI_API_KEY);
+  // console.log(process.env.REACT_APP_OPEINAI_API_KEY);
+  console.log(arrayItems);
   return (
     <div className="App">
-      <OptionSelection/>
+      <OptionSelection arrayItems={arrayItems} />
     </div>
   );
 }
--- src/components/OptionSelection.js
+++ src/components/OptionSelection.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
-function OptionSelection(props) {
-    return (
-        <>
-        <h1 className='heading' >React AI APP</h1>
-        </>
-    );
+function OptionSelection({ arrayItems }) {
+  return (
+    <>
+      <h1 className="heading">React AI APP</h1>
+
+      <div className="grid-main">
+        {arrayItems.map((item) => {
+          return (
+            <div key={item.id} className="grid-child">
+              <h3>{item.name} </h3>
+              <p>{item.description} </p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default OptionSelection;
