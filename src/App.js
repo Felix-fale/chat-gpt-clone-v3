10c78ead33e6f48d2011cef2a9ebe5058b9488d4
Felix-fale <falefelix23@gmail.com>
2023-01-24 22:30:59 +0000

    feat: Importing the translated component

--- src/App.js
+++ src/App.js
@@ -7,20 +7,32 @@ import { useState } from "react";
 function App() {
   const [option, setOption] = useState({});
 
+  const [input, setInput] = useState("")
+
   // console.log(process.env.REACT_APP_OPEINAI_API_KEY);
   // console.log(arrayItems);
 
   const selectOption = (option) => {
     setOption(option);
   };
-  console.log(Object.values(option));
+
+  const doStuff =()=>{
+
+  }
+
+  console.log(input);
 
   return (
     <div className="App">
-      <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
-      <Translation />
+      {Object.values(option).length === 0 ? (
+        <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
+      ) : (
+        <Translation doStuff={doStuff} setInput={setInput} />
+      )}
     </div>
   );
 }
 
 export default App;
+
+// https://www.youtube.com/watch?v=98bGwOYoEGg&list=WL&index=8&t=1697s   42m52s
--- src/components/Translation.js
+++ src/components/Translation.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
-function Translation(props) {
-    return (
-        <div>
-            Translation
-        </div>
-    );
+function Translation({ doStuff, setInput }) {
+  return (
+    <div>
+      <textarea
+        className="text-area"
+        rows={10}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <br />
+      <button className="action-btn" onClick={doStuff}>
+        DO YOU STUFF!
+      </button>
+    </div>
+  );
 }
 
 export default Translation;
